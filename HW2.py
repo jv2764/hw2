ed252201c50f84f8ad990a571d35967107c256c5
John Vu <31754493+jv2764@users.noreply.github.com>
2017-09-29 22:46:41 -0700

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,49 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+user enter a number
+if number entered is greater than 100
+print message "Found a number greater than 100"
+
+
+# In[ ]:
+
+user enter a name
+if name entered is Joe
+print message " User Joe was entered"
+if name entered is not Joe
+print message "Use Joe was not entered"
+
+
+# In[ ]:
+
+user enter salary
+if salary entered is less than 70,000
+print message "User Salary is less than 70,000"
+if salary entered is less than 100,000
+print message "User Salart is less than 100,000"
+otherwise print message "User Salary is greater than or equal to 100,000"
+
+
+# In[ ]:
+
+get a number from user
+Find the sum of all numbers from 1 to the number that the user entered.
+if the user enters 10, then  find the sum of all numbers from 1 to 10 
+user range statement
+print results
+
+
+
+# In[ ]:
+
+MyNumber ==9
+use while loop, ask user to to enter a number
+if the number entered by the user matches the myNumber 
+If there is a match, break from the while loop
+print "Yippee! User guessed the right number"
+If there is no match allow the while loop to continue to ask the user for a number until a match is found
+
+
